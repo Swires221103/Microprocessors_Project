54ec2eb12bb7064943e1f1217a9c5a7de3e735bd
Swires221103 <jack.swires@outlook.com>
2024-10-31 10:58:53 +0000

    Altered to do different loops depending on Port D input

--- main.s
+++ main.s
@@ -10,15 +10,33 @@ main:
 start:
 	movlw 	0x0
 	movwf	TRISC, A	    ; Port C all outputs
-	bra 	test
-loop:
+	movlw	0xff
+	movwf	TRISD, A
+	movlw	0x0
+	movwf	0x6, A
+	btfsc	PORTD, 0, A
+	bra	loop1
+	btfsc	PORTD, 1, A
+	bra	loop2
+	goto	start
+	
+loop1:
 	movff 	0x06, PORTC
 	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+test1:
+	movwf	0x06, A		    ; Test for end of loop condition
+	movlw 	0x30
 	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
+	bra 	loop1		    ; Not yet finished goto start of loop again
+	goto 	0x0		    ; Re-run program from start
+loop2:
+	movff 	0x06, PORTC
+	incf 	0x06, W, A
+test2:
+	movwf	0x06, A		    ; Test for end of loop condition
+	movlw 	0x20
+	cpfsgt 	0x06, A
+	bra 	loop2		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 
 	end	main
